df56bca68e1f0f1c19e8db73ae0e5d5d5f97cb95
Ramirez Diaz Victor Francisco <franvictor18@gmail.com>
2023-10-23 21:57:15 -0300

    desafios clase 19

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const pathPublic = path.join(__dirname, '/public');
 server.use(express.static(pathPublic));
 
 server.listen(3030, () => {
-    console.log("Servidor corriendo");
+    console.log("Servidor corriendo en http://localhost:3030/");
 });
 
 server.get('/', (req, res) => {
