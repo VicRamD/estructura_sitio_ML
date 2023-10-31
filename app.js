6f787a3ee21c56959ba265b63f0121b15971e516
Víctor Francisco Ramírez Díaz <franvictor18@gmail.com>
2023-10-30 21:36:16 -0300

    Ejercicios clase 21

--- app.js
+++ app.js
@@ -14,4 +14,19 @@ server.listen(3030, () => {
 server.get('/', (req, res) => {
     let pathHome = path.join(__dirname, '/views/home.html');
     res.sendFile(pathHome);
-})
+});
+
+server.post('/', (req, res) => {
+    let pathHome = path.join(__dirname, '/views/home.html');
+    res.sendFile(pathHome);
+});
+
+server.get('/register', (req, res)=>{
+    let pathRegister = path.join(__dirname, '/views/register.html');
+    res.sendFile(pathRegister);
+});
+
+server.get('/login', (req, res)=>{
+    let pathLogin = path.join(__dirname, '/views/login.html');
+    res.sendFile(pathLogin);
+});
