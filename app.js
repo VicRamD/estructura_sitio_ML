2736f7bee7af6cabfe73ca351c2a3f822089a949
Ramirez Diaz Victor Francisco <franvictor18@gmail.com>
2023-11-06 09:17:22 -0300

    configuraciones previas a la clase 23

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ const pathPublic = path.join(__dirname, '/public');
 
 server.use(express.static(pathPublic));
 
-server.listen(3030, () => {
-    console.log("Servidor corriendo en http://localhost:3030/");
+const port = process.env.PORT || 3030
+
+server.listen(port, () => {
+    /*console.log(`Servidor corriendo en http://localhost:3030/`); */
+    console.log(`Servidor corriendo en ${port}`);
 });
 
 server.get('/', (req, res) => {
